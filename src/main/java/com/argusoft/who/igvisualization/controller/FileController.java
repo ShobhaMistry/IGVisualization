7ba1b3a0e49aa0d9de9871e5aa99cc83cb778019
Shobha <smistry@argusoft.com>
2023-05-24 18:41:54 +0530

    resolved cors policy issue

--- src/main/java/com/argusoft/who/igvisualization/controller/FileController.java
+++ src/main/java/com/argusoft/who/igvisualization/controller/FileController.java
@@ -3,6 +3,7 @@ package com.argusoft.who.igvisualization.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.argusoft.who.igvisualization.helper.FileUploadHelper;
 import com.fasterxml.jackson.databind.JsonNode;
+
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 public class FileController {
