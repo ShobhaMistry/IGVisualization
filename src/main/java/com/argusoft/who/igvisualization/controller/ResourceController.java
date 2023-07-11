5d901eb563c17c99069a58c9fad7c60804c8c28e
smistry@argusoft.com <smistry@argusoft.com>
2023-07-11 10:39:43 +0530

    code refactoring

--- src/main/java/com/argusoft/who/igvisualization/controller/ResourceController.java
+++ src/main/java/com/argusoft/who/igvisualization/controller/ResourceController.java
@@ -19,30 +19,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class ResourceController {
 
     @Autowired
-    PlanDefinitionService planDefinitionService;
-
-    @Autowired
-    ActivityDefinitionService activityDefinitionService;
-
-    @Autowired
-    QuestionnaireService questionnaireService;
-
-    @Autowired
-    StructureMapService structureMapService;
-
-    @Autowired
-    StructureDefinitionService structureDefinitionService;
-
-    @Autowired
-    LibraryService libraryService;
-
-    @Autowired
-    ValueSetService valuesetService;    
-
-    @Autowired
-    CodeSystemService codeSystemService;
-
-    @Autowired
     ResourceService resourceService;
 
     @GetMapping("/allResources")
@@ -53,101 +29,6 @@ public class ResourceController {
     @GetMapping("/getResource/{selectedResource}")
     public List<JsonNode> getResource(@PathVariable String selectedResource){
         return resourceService.getResource(selectedResource);
-    }
-
-    @GetMapping("/planDefinition")
-    public List<JsonNode> getPD() throws IOException {
-        return planDefinitionService.getPlanDefinition();
-    }
-
-    @GetMapping("/planDefinition/{id}")
-    public JsonNode getPlanDefinitonById(@PathVariable String id){
-        return planDefinitionService.getPlanDefinitionById(id);
-    }
-
-    @GetMapping("/planDefinition/{pdId}/actions")
-    public JsonNode getActionsURL(@PathVariable String pdId) throws IOException {
-        return planDefinitionService.getActions(pdId);
-    }
-
-    @GetMapping("/planDefinition/{pdId}/actions/{id}")
-    public JsonNode getActionById(@PathVariable String pdId,@PathVariable String id){
-        return planDefinitionService.getActionById(pdId,id);
-    }
-
-    @GetMapping("/activityDefinition")
-    public List<JsonNode> getAllActivityDefinition() {
-        return activityDefinitionService.getAllActivityDefinition();
-    }
-
-    @GetMapping("/activityDefinition/{id}")
-    public JsonNode getActivityDefinitionByID(@PathVariable String id) {
-        return activityDefinitionService.getActivityDefinitionById(id);
-    }
-
-    @GetMapping("/questionnaire")
-    public List<JsonNode> getAllQuestionnaire(){
-        return questionnaireService.getAllQuestionnaire();
-    }
-
-    @GetMapping("/questionnaire/{id}")
-    public JsonNode getQuestionnaireById(@PathVariable String id) {
-        return questionnaireService.getQuestionnaireById(id);
-    }
-
-    @GetMapping("/structureMap")
-    public List<JsonNode> getAllStructureMapBy() {
-        return structureMapService.getAllStructureMap();
-    }
-
-    @GetMapping("/structureMap/{id}")
-    public JsonNode getStructureMapById(@PathVariable String id) {
-        return structureMapService.getStructureMapById(id);
-    }
-
-    @GetMapping("/targetFromStructureMap/{id}")
-    public List<JsonNode> getTargetFromStructureMap(@PathVariable String id) {
-        return structureMapService.getTargetFromStructureMap(id);
-    }
-
-    @GetMapping("/structureDefinition")
-    public List<JsonNode> getAllStructureDefinition() {
-        return structureDefinitionService.getAllStructureDefinition();
-    }
-
-    @GetMapping("/structureDefinition/{id}")
-    public JsonNode getStructureDefinitionById(@PathVariable String id) {
-        return structureDefinitionService.getStructureDefinitionById(id);
-    }
-
-    @GetMapping("/library")
-    public List<JsonNode> getAllLibrary(){
-        return libraryService.getAllLibrary();
-    }
-
-    @GetMapping("/library/{id}")
-    public JsonNode getLibraryById(@PathVariable String id){
-        return libraryService.getLibraryById(id);
-    }
-
-    @GetMapping("/valueSet")
-    public List<JsonNode> getAllValueSet(){
-        return valuesetService.getAllValueSet();
-    }
-
-    @GetMapping("/valueSet/{id}")
-    public JsonNode getValueSetById(@PathVariable String id){
-        return valuesetService.getValueSetById(id);
-    }
-
-    @GetMapping("/code")
-    public JsonNode getAllCode(){
-        return codeSystemService.getAllCode();
-    }
-
-    @GetMapping("/code/{id}")
-    public JsonNode getCodeById(@PathVariable String id){
-        return codeSystemService.getCodeById(id);
     }
 
 }
